99b9998b8e21e06cf2fec5f08594ec7e4a68a403
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2022-11-27 01:26:19 +0300

    Odev3 directory Changed

--- app/src/main/java/com/samet/androidstudiobootcampodevler/Odev3/Odev3.java
+++ app/src/main/java/com/samet/androidstudiobootcampodevler/Odev3/Odev3.java
@@ -1,8 +1,10 @@
-package com.samet.androidstudiobootcampodevler;
+package com.samet.androidstudiobootcampodevler.Odev3;
 
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.samet.androidstudiobootcampodevler.R;
 
 public class Odev3 extends AppCompatActivity {
 
