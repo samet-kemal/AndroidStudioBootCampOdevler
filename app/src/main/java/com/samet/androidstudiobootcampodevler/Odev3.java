108f9a877b710d3dea73d1c9aab3edc8e1a9fcbc
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2022-11-27 01:23:59 +0300

    Odev3 Activity Generated

---
+++ app/src/main/java/com/samet/androidstudiobootcampodevler/Odev3.java
@@ -0,0 +1,14 @@
+package com.samet.androidstudiobootcampodevler;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Odev3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_odev3);
+    }
+}
