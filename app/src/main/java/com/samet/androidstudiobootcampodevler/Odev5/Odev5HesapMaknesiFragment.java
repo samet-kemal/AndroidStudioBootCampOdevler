d60be89b89c0f65dc8959825fe2730600090d531
samet-kemal <77715935+samet-kemal@users.noreply.github.com>
2022-12-05 01:10:48 +0300

    Multiple Digit Added

--- app/src/main/java/com/samet/androidstudiobootcampodevler/Odev5/Odev5HesapMaknesiFragment.java
+++ app/src/main/java/com/samet/androidstudiobootcampodevler/Odev5/Odev5HesapMaknesiFragment.java
@@ -13,7 +13,7 @@ import com.samet.androidstudiobootcampodevler.databinding.FragmentOdev5HesapMakn
 public class Odev5HesapMaknesiFragment extends Fragment {
 
     FragmentOdev5HesapMaknesiBinding binding;
-    double toplam=0;
+    int toplam=0;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -23,47 +23,79 @@ public class Odev5HesapMaknesiFragment extends Fragment {
         binding= FragmentOdev5HesapMaknesiBinding.inflate(inflater, container, false);
 
         binding.button0.setOnClickListener(view->{
-            binding.editTextNumber.setText("0");
-        });
+            if (Double.parseDouble(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("0");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"0");
+            }        });
 
         binding.button1.setOnClickListener(view->{
-            binding.editTextNumber.setText("1");
+            if (Double.parseDouble(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("1");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"1");
+            }
+
 
         });
         binding.button2.setOnClickListener(view->{
-            binding.editTextNumber.setText("2");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("2");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"2");
+            }
         });
         binding.button3.setOnClickListener(view->{
-            binding.editTextNumber.setText("3");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("3");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"3");
+            }
         });
         binding.button4.setOnClickListener(view->{
-            binding.editTextNumber.setText("4");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("4");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"4");
+            }
         });
         binding.button5.setOnClickListener(view->{
-            binding.editTextNumber.setText("5");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("5");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"5");
+            }
         });
         binding.button6.setOnClickListener(view->{
-            binding.editTextNumber.setText("6");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("6");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"6");
+            }
         });
         binding.button7.setOnClickListener(view->{
-            binding.editTextNumber.setText("7");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("7");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"7");
+            }
         });
         binding.button8.setOnClickListener(view->{
-            binding.editTextNumber.setText("8");
-
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("8");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"8");
+            }
         });
         binding.button9.setOnClickListener(view->{
-            binding.editTextNumber.setText("9");
-        });
+            if (Integer.valueOf(binding.editTextNumber.getText().toString())==0){
+                binding.editTextNumber.setText("9");
+            }else{
+                binding.editTextNumber.setText(binding.editTextNumber.getText().toString()+"9");
+            }        });
 
         binding.buttonTopla.setOnClickListener(view->{
-            double sayi=Double.parseDouble(binding.editTextNumber.getText().toString());
+            int sayi=Integer.valueOf(binding.editTextNumber.getText().toString());
             toplam=toplam+sayi;
             binding.editTextNumber.setText(""+toplam);
 
@@ -76,7 +108,11 @@ public class Odev5HesapMaknesiFragment extends Fragment {
 
         binding.buttonEsittir.setOnClickListener(view->{
 
-            binding.editTextNumber.setText("Sonuc:"+toplam);
+            binding.editTextNumber2.setText("Toplam="+toplam);
+        });
+
+        binding.buttonSil.setOnClickListener(view->{
+            binding.editTextNumber.setText("0");
         });
 
         return binding.getRoot();
